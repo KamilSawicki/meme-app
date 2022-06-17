ed1e928dcc860217c051397045c7f0ba019dc9d0
k.sawicki <k.sawicki@ideo.pl>
2022-06-17 20:54:30 +0200

    override toArray in comments, fix code styling

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,47 +6,56 @@ use App\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
-    use HasFactory, HasUuid;
+class Comment extends Model {
+	use HasFactory, HasUuid;
 
-    protected $attributes = [
-        'created_by_id' => null,
-        'updated_by_id' => null,
-        'deleted_by_id' => null,
-        'created_at' => null,
-        'updated_at' => null,
-        'deleted_at' => null,
-        'meme_id' => null,
-        'comment_id' => null,
-        'content' => null,
-    ];
+	protected $attributes = [
+		'created_by_id' => null,
+		'updated_by_id' => null,
+		'deleted_by_id' => null,
+		'created_at' => null,
+		'updated_at' => null,
+		'deleted_at' => null,
+		'meme_id' => null,
+		'comment_id' => null,
+		'content' => null,
+	];
 
-    protected $casts = [
-        'created_at' => 'datetime',
-        'updated_at' => 'datetime',
-        'deleted_at' => 'datetime',
-    ];
+	protected $casts = [
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime',
+		'deleted_at' => 'datetime',
+	];
 
-    //relations
-    public function createdBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(User::class, 'created_by');
-    }
-    public function updatedBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(User::class, 'updated_by');
-    }
-    public function deletedBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(User::class, 'deleted_by');
-    }
-    public function meme(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(Meme::class, 'meme_id');
-    }
-    public function comment(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(Comment::class, 'comment_id');
-    }
+	//relations
+	public function createdBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
+		return $this->belongsTo(User::class, 'created_by_id');
+	}
+
+	public function updatedBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
+		return $this->belongsTo(User::class, 'updated_by_id');
+	}
+
+	public function deletedBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
+		return $this->belongsTo(User::class, 'deleted_by_id');
+	}
+
+	public function meme(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
+		return $this->belongsTo(Meme::class, 'meme_id');
+	}
+
+	public function comment(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
+		return $this->belongsTo(Comment::class, 'comment_id');
+	}
+
+	//overrides
+	public function toArray(): array {
+		$array = parent::toArray();
+		$array['created_by'] = $this->createdBy->toArray();
+		$array['updated_by'] = $this->updatedBy->toArray();
+		$array['deleted_by'] = $this->deletedBy->toArray();
+		$array['meme'] = $this->meme->toArray();
+		$array['comment'] = $this->comment->toArray();
+		return $array;
+	}
 }
--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -6,14 +6,13 @@ use App\Enums\VoteEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Vote extends Model
-{
-    use HasFactory;
+class Vote extends Model {
+	use HasFactory;
 
-    protected $casts = [
-        'created_at' => 'datetime',
-        'update_at' => 'datetime',
-        'deleted_at' => 'datetime',
-        'vote' => VoteEnum::class,
-    ];
+	protected $casts = [
+		'created_at' => 'datetime',
+		'update_at' => 'datetime',
+		'deleted_at' => 'datetime',
+		'vote' => VoteEnum::class,
+	];
 }
